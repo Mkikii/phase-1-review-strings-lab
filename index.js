42db48aea263dfb9bf9ee05027bd5b99fbb00fb2
Molly <maureenwkarimi@gmail.com>
2025-06-05 13:05:47 +0300

    Updated index.js and added my-test.html

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Define currentUser = 'Grace Hopper'
+const currentUser= 'Grace Hopper';
+
+// Create welcome message using template literals
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Create excited welcome message (uppercase version)
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Create short greeting(first letter only)
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
